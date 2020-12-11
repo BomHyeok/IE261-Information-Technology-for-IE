da7393f5da845177905ea5d7f203b667012944d4
BomHyeok <59109668+BomHyeok@users.noreply.github.com>
2020-12-11 15:50:41 +0900

    Create homework3.php

---
+++ HOMEWORK 3/homework3.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+  <title>2017 Annual Horoscope</title>
+  </head>
+  <body bgcolor="black" text="white">
+    <table>
+      <tr>
+      <td><img src="http://www.123newyear.com/wp-content/uploads/2015/08/horoscope-2016.jpg" width="400" height="400"></td>
+      <td>
+        <form method="get" action="homework3-1.php">
+          <font size="5">Name : </font>
+          <input type="text" name="name" size="20"><br><br>
+          <font size="5">Gender : </font>
+          <input type="radio" value="1" name="gender"><font size="5">Male</font>
+          <input type="radio" value="2" name="gender"><font size="5">Female</font><br><br>
+          <font size="5">Date of Birth : </font>
+          <input type="text" name="day" size="3" maxlength="2">day/ 
+          <input type="text" name="month" size="3" maxlength="2">month<br><br>
+          <input type="submit" value="Show my horoscope">
+        </form></td>
+      </tr>
+    </table>
+  </body>
+</html>
